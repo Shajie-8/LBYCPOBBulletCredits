5c5621f1ee997d56d7d6569faf387f556cfb2cf5
Shajie-8 <muhammad_macabalang@dlsu.edu.ph>
2026-08-03 08:12:11 +0800

    Added name in Final Boss

--- src/main/java/ph/edu/dlsu/lbycpob/lbycpobbulletcredits/entity/PlayerComponent.java
+++ src/main/java/ph/edu/dlsu/lbycpob/lbycpobbulletcredits/entity/PlayerComponent.java
@@ -20,7 +20,7 @@ import static com.almasb.fxgl.dsl.FXGL.getAppHeight;
  */
 public class PlayerComponent extends ShipComponent {
 
-    private static final int STARTING_LIVES = 14;
+    private static final int STARTING_LIVES = 18;
     private static final int HEALTH_PER_LIFE = 1;
     private static final double FIRE_INTERVAL_SECONDS = 0.12;
     private static final double BULLET_SPEED = 620;
